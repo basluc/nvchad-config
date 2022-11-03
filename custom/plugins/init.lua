f1035db9cf1f821d05faa1f977e029860115278b
basluc <bastien.aufrand@gmail.com>
2022-11-03 11:16:16 +0100

    Add diffview with config to run with nvchad

--- custom/plugins/init.lua
+++ custom/plugins/init.lua
@@ -3,39 +3,45 @@
 local pluginConfs = require "custom.plugins.configs"
 
 return {
-  ['hrsh7th/vim-vsnip'] = {},
-  ['hrsh7th/vim-vsnip-integ'] = {},
+    ['hrsh7th/vim-vsnip'] = {},
+    ['hrsh7th/vim-vsnip-integ'] = {},
 
-  --Override
-  ["nvim-treesitter/nvim-treesitter"] = {
-    override_options = pluginConfs.treesitter
-  },
+    --Override
+    ["nvim-treesitter/nvim-treesitter"] = {
+        override_options = pluginConfs.treesitter
+    },
 
-  ["williamboman/mason.nvim"] = {
-    override_options = {
-      ensure_installed = {
-        "lua-language-server",
-        "pyright",
-        "rust-analyzer",
-        "intelephense",
-        "html-lsp",
-        "css-lsp",
-        "dockerfile-language-server",
-      }
-    }
-  },
+    ["williamboman/mason.nvim"] = {
+        override_options = {
+            ensure_installed = {
+            "lua-language-server",
+            "pyright",
+            "rust-analyzer",
+            "intelephense",
+            "html-lsp",
+            "css-lsp",
+            "dockerfile-language-server",
+            }
+        }
+    },
 
-  ["neovim/nvim-lspconfig"] = {
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.plugins.lspconfig"
-    end,
-  },
+    ["neovim/nvim-lspconfig"] = {
+        config = function()
+            require "plugins.configs.lspconfig"
+            require "custom.plugins.lspconfig"
+        end,
+    },
 
 
 
-  -- ['mhinz/vim-startify'] = {},
-  -- ['nvim-lua/plenary.nvim'] = {},
-  ['sindrets/diffview.nvim'] = {},
+    -- ['mhinz/vim-startify'] = {},
+    -- ['nvim-lua/plenary.nvim'] = {},
+     ["sindrets/diffview.nvim"] = {
+          after = "plenary.nvim",
+          config = function()
+             require("diffview").setup()
+          end,
+       },
 
+     ["nvim-lua/plenary.nvim"] = {rm_default_opts = true },
 }
