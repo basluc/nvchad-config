46b80598df0e9bd1167b8ed936d70f511358cf1e
basluc <bastien.aufrand@gmail.com>
2023-06-09 01:12:14 +0200

    auto-session update

--- custom/plugins/init.lua
+++ custom/plugins/init.lua
@@ -38,7 +38,19 @@ return {
 
     {
         "rmagatti/auto-session",
-        lazy = false
+        lazy = false,
+        config = function ()
+            require("auto-session").setup {
+                  log_level = "error",
+                  cwd_change_handling = {
+                    restore_upcoming_session = true, -- already the default, no need to specify like this, only here as an example
+                    pre_cwd_changed_hook = nil, -- already the default, no need to specify like this, only here as an example
+                    -- post_cwd_changed_hook = function() -- example refreshing the lualine status line _after_ the cwd changes
+                    --   require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
+                    -- end,
+                },
+            }
+        end
     },
 
     {
